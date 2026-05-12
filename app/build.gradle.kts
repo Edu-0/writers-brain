8c321ec4a88e0ab7f73de1abd203eabfa802ae9f
Edu-0 <eduardojose.de.souza332@gmail.com>
2026-05-12 16:19:26 -0300

    feat: activity_main e card de mundo finalizado. Iniciando tela de criar "novo mundo"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,10 @@ android {
 dependencies {
     implementation(libs.androidx.activity.ktx)
     implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.cardview)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.recyclerview)
     implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
