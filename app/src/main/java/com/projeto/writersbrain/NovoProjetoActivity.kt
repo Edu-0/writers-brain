c482705ea8bef163c45bef62bce68e30bfb06b78
Edu-0 <eduardojose.de.souza332@gmail.com>
2026-05-13 16:33:43 -0300

    feat: adicionando lógica de criação de mundo

--- app/src/main/java/com/projeto/writersbrain/NovoProjetoActivity.kt
+++ app/src/main/java/com/projeto/writersbrain/NovoProjetoActivity.kt
@@ -1,8 +1,11 @@
 package com.projeto.writersbrain
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.projeto.writersbrain.model.Mundo
 
 class NovoProjetoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +13,17 @@ class NovoProjetoActivity : AppCompatActivity() {
         enableEdgeToEdge()
 
         setContentView(R.layout.activity_novo_projeto)
+
+        val buttonCriar = findViewById<FloatingActionButton>(R.id.fabCriar)
+
+        val titulo = findViewById<TextView>(R.id.etTitulo)
+        val descricao = findViewById<TextView>(R.id.etDescricao)
+        val tags = findViewById<TextView>(R.id.etTags)
+        val instrucoes = findViewById<TextView>(R.id.etInstrucoes)
+
+        buttonCriar.setOnClickListener {
+            finish()
+        }
+
     }
 }
